5f5b0050ba5a66946e4844a6103a8a218ca08726
tarekhossain90267-del <tarek.hossain90267@gmail.com>
2026-07-13 10:10:59 +0600

    Create 0004-Median of Two sorted Arrays.py

---
+++ Arrays/0004-Median of Two sorted Arrays.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        A, B = nums1, nums2
+        m, n = len(A), len(B)
+
+        total = m + n
+        half = (total + 1) // 2
+
+        left, right = 0, m
+
+        while left <= right:
+            i = (left + right) // 2
+            j = half - i
+
+            A_left = A[i - 1] if i > 0 else float("-inf")
+            A_right = A[i] if i < m else float("inf")
+
+            B_left = B[j - 1] if j > 0 else float("-inf")
+            B_right = B[j] if j < n else float("inf")
+
+            if A_left <= B_right and B_left <= A_right:
+                if total % 2 == 1:
+                    return max(A_left, B_left)
+
+                return (max(A_left, B_left) + min(A_right, B_right)) / 2.0
+
+            elif A_left > B_right:
+                right = i - 1
+            else:
+                left = i + 1   
